f91f31ebfc0baef2e10e9719acab08d38742b455
ghedir_ghada <ghedirghada77@gmail.com>
2025-02-28 10:26:20 +0100

    first version Home.page AdmissionChart stastCards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-        <h1>Bienvenue sur PredictHealth</h1>
-        <p>Page d'accueil du Dashboard</p>
-    </div>
+  <v-container fluid>
+    <StatsCards />
+    <v-row class="equal-height" align="stretch">
+      <v-col cols="12" md="8">
+        <AdmissionsChart class="full-height" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <AdmissionsComparison class="full-height" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
+import StatsCards from "../components/StatsCards.vue"
+import AdmissionsChart from "../components/AdmissionsChart.vue"
+import AdmissionsComparison from "../components/AdmissionsComparison.vue"
 </script>
 
 <style scoped>
+.equal-height {
+  display: flex;
+}
+
+.full-height {
+  height: 100%;
+}
 </style>
