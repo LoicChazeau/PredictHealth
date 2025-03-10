ef824a23cf01f54b2cabeccbc86ddae9eef56d05
Loic Chazeau <loic.chazeau@epitech.digital>
2025-03-10 23:44:20 +0100

    add fastapi with server.py

---
+++ src/api/server.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+# Charger les données
+df = pd.read_csv("Global_dataset.csv")
+
+# Endpoint pour récupérer toutes les données
+@app.get("/data")
+def get_data():
+    return df.to_dict(orient="records")
+
+# Endpoint pour récupérer une prédiction basée sur l’algorithme
+@app.get("/predict/{date}")
+def predict(date: str):
+    # ICI, APPELER LA FONCTION D’ANALYSE PREDICTIVE DU NOTEBOOK
+    prediction = {
+        "date": date,
+        "prediction": 125
+    }
+    return prediction
+
+# Lancer le serveur API
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
